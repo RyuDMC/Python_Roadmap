e8826bb7d45b427d5c9fd9e63b0d7767de19e29b
RyuDMC <oliver.a.reyes271@gmail.com>
2026-03-08 10:39:30 -0400

    se actualizo el archivo main.py

--- GitHub_User_Activity/main.py
+++ GitHub_User_Activity/main.py
@@ -1,4 +1,11 @@
 import json
 import requests
 
+print("Escriba su nombre de usuario: ")
+username = str(input())
 
+response = requests.get("https://api.github.com/users/{}/events".format(username))
+
+if response.status_code == 200:
+  data = json.loads(response.text)
+  print(data)
