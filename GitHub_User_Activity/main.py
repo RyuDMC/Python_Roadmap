3f3f4162ffe1feecdc21b4ddb803fa522099562e
RyuDMC <oliver.a.reyes271@gmail.com>
2026-03-14 19:49:16 -0400

    main.py

--- GitHub_User_Activity/main.py
+++ GitHub_User_Activity/main.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from collections import defaultdict
 
 print("Escriba su nombre de usuario: ")
 username = str(input())
@@ -7,6 +8,5 @@ username = str(input())
 response = requests.get("https://api.github.com/users/{}/events/public".format(username))
 
 if response.status_code == 200:
-  with open("/media/ryudmc/17bd76d4-00a3-4be2-a374-79313809c703/Oliver/Programing/Codes/Python/Python_Roadmap/GitHub_User_Activity/data.json", "w") as new_data:
-    data = json.dumps(json.loads(response.text))
-    json.dump(data, new_data)
+  data = json.loads(response.text)
+  resume = defaultdict(lambda: defaultdict(int))
