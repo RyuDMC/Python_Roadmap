4b9a834a54cdc6ebc12fa6f939260e41c278d6cd
RyuDMC <oliver.a.reyes271@gmail.com>
2026-03-24 00:03:46 -0400

    commit

--- GitHub_User_Activity/aux_functions.py
+++ GitHub_User_Activity/aux_functions.py
@@ -34,17 +34,13 @@ def add_event(eventos):
     elif tipo == "CreateEvent":
       created = evento["payload"].get("ref_type", "")
       if created == "branch":
-        resume[repo_name]["branch"] += 1
-      elif created == "repository":
-        resume[repo_name]["repository"] += 1
+        resume[repo_name]["created_branch"] += 1
       elif created == "tag":
-        resume[repo_name]["tag"] += 1
+        resume[repo_name]["created_tag"] += 1
     elif tipo == "RemoveEvent":
       removed = evento["payload"].get("ref_type", "")
       if removed == "branch":
-        resume[repo_name]["branch"] += 1
-      elif removed == "repository":
-        resume[repo_name]["repository"] += 1
+        resume[repo_name]["removed_branch"] += 1
     elif tipo == "EventDiscussion":
       resume[repo_name]["discussion"] += 1
     elif tipo == "GollumEvent":
@@ -71,41 +67,55 @@ def add_event(eventos):
       resume[repo_name]["forked"] += 1
     elif tipo == "MemberEvent":
       resume[repo_name]["member"] += 1
-    else:
-      print(tipo)
   return resume
 
 def show(resume):
   for repo, actions in resume.items():
     if actions.get("pushes"):
       print(f" - Pushed {actions["pushes"]} commits to {repo}")
-    if actions.get("pull_request"):
-      print(f" - Opened{actions["pull_request_opened"]} pull requests in {repo}")
-      print(f" - Closed{actions["pull_request_closed"]} pull requests in {repo}")
+    if actions.get("pull_request_opened"):
+      print(f" - Opened {actions["pull_request_opened"]} pull requests in {repo}")
+    elif actions.get("pull_request_closed"):
+      print(f" - Closed {actions["pull_request_closed"]} pull requests in {repo}")
+    elif actions.get("pull_request_merged"):
+      print(f" - Merged {actions["pull_request_merged"]} pull requests in {repo}")
+    elif actions.get("pull_request_reopend"):
+      print(f" - Reopened {actions["pull_request_reopened"]} pull requests in {repo}")
+    elif actions.get("pull_request_assigned"):
+      print(f" - Assigned {actions["pull_request_assigned"]} pull requests in {repo}")
+    elif actions.get("pull_request_labeled"):
+      print(f" - Labeled {actions["pull_request_labeled"]} pull requests in {repo}")
+    elif actions.get("pull_request_unassigned"):
+      print(f" - Unassigned {actions["pull_request_unassigned"]} pull requests in {repo}")
+    elif actions.get("pull_request_unlabeled"):
+      print(f" - Unlabeled {actions["pull_request_unlabeled"]} pull requests in {repo}")
     if actions.get("commits"):
       print(f" - Pushed {actions["commits"]} commits to {repo}")
     if actions.get("stars"):
-      print(f" - {repo} received {actions["stars"]} stars")
-    if actions.get("create"):
-      for created in actions["create"]:
-        print(f"The {created} was create in {repo}")
-    if actions.get("remove"):
-      for removed in actions["remove"]:
-        print(f"The {removed} was remove from {repo}")
-    if actions.get("DiscussionEvent"):
-      print(f"Created {actions["discussion"]} discussions in {repo}")
-    if actions.get("GollumEvents"):
-      print(f"Created {actions["page"]} pages in {repo}")
+      print(f" - The repository {repo} received {actions["stars"]} stars")
+    if actions.get("discussion"):
+      print(f" - Created {actions["discussion"]} discussions in {repo}")
     if actions.get("IssueCommentEvent"):
       print(f" - Pushed {actions["issuecomment"]} issuecomments to {repo}")
-    if actions.get("issues"):
-      print(f"{actions["issues"]["opened"]} had open in {repo}")
-      print(f"{actions["issues"]["closed"]} had close in {repo}")
-      print(f"{actions["issues"]["reopened"]} had reopen in {repo}")
-    if actions.get("member"):
-      for member in actions["members"]:
-        print(f"added {member} to {repo}")
+    if actions.get("issues_opened"):
+      print(f" - {actions["issues_opened"]} issues were opened in {repo}")
+    if actions.get("issues_reopened"):
+      print(f" - {actions["issues_reopened"]} issues were reopened in {repo}")
+    if actions.get("issues_closed"):
+      print(f" - {actions["issues_closed"]} issues were closed in {repo}")
     if actions.get("public"):
-      print(f"The {repo} became public")
+      print(f" - The repository {repo} became public")
     if actions.get("launched"):
-      print(f"The {repo} was launched")
+      print(f" - The repository {repo} was launched")
+    if actions.get("member"):
+      print(f" - The repository {repo} has {actions["member"]}")
+    if actions.get("created_branch"):
+      print(f" - The repository {repo} has {actions["created_branch"]} branchs")
+    if actions.get("created_tag"):
+      print(f" - The repository {repo} has {actions["created_tag"]} tags")
+    if actions.get("removed_branch"):
+      print(f" - The repository {repo} removed {actions["removed_branch"]} branchs")
+    if actions.get("created_page"):
+      print(f" - The repository {repo} has {actions["created_page"]} pages created")
+    if actions.get("edited_page"):
+      print(f" - The repository {repo} has {actions["edited_page"]} pages edited")
